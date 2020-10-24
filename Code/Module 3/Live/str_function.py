6a814859c9a5b31d5087db06791ef119eb7e744a
Pavel Tsytovich <pavel.tsytovich@gmail.com>
2020-10-24 22:08:48 +0300

    Строки и оператор slice

---
+++ Code/Module 3/Live/str_function.py
@@ -0,0 +1,11 @@
+s1 = "THIS "
+s2 = " String and String"
+
+print(s1 + s2)
+print(s1 * 3)
+print(s1[3])
+print(len(s1))
+print(s1.find('IS'))
+print(s2.rfind('tr'))
+print(s1.index('IS'))
+print(s2.replace('String','TEST'))
