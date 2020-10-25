b5bd51327074fd16aaae1a09d46039c59e0cff3c
Pavel Tsytovich <pavel.tsytovich@gmail.com>
2020-10-25 18:44:59 +0300

    Списки и кортежи

---
+++ Code/Module 3/Live/slice.py
@@ -0,0 +1,10 @@
+str1 = "123456789"
+print('str1 = "123456789"')
+print("str1[1] = ", str1[1])
+print("str1[2:5] = ", str1[2:5])
+print("str1[-2] = ", str1[-2])
+print("str1[3:] = ", str1[3:])
+print("str1[:3] = ", str1[:3])
+print("str1[:] = ", str1[:])
+print("str1[-1:-5:-1]",str1[-1:-5:-1])
+print("str1[5:1:-1]",str1[5:1:-1])
---
+++ Code/let-tuple.py
@@ -0,0 +1,9 @@
+x = [1,2,3,4]
+a,b,c,d = x
+print(a,b,c,d)
+
+q = 2
+w = 4
+
+q, w = w, q
+print(q,w)
