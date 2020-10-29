604e1cf00057d44af8c22a9f1aab87307ca3e9eb
Pavel Tsytovich <pavel.tsytovich@gmail.com>
2020-10-29 23:44:27 +0300

    окончательный вариант демобота первой части

--- Runner/DemoBot/DemoBot.py
+++ Runner/DemoBot/DemoBot.py
@@ -12,18 +12,33 @@ class DemoBot:
    
     def __init__(self):
         self.__bot = Bot(config.bot_id)
+        self.__handlers = {}
+        self.__disp = Dispatcher(bot=self.__bot)
         
     async def __main(self):      
-        try:
-            disp = Dispatcher(bot=self.__bot)
-            disp.register_message_handler(self.start_handler,commands = {"start","restart"})
-            await disp.start_polling()
+        try:            
+            await self.__disp.start_polling()
         finally:
             await self.__bot.close()
                     
-    async def start_handler(self,event):
-        await event.answer(f"Hello {event.from_user.get_mention(as_html=True)} :-)",parse_mode = types.ParseMode.HTML)
-  
+    async def __start_handler(self,event):
+        handler = self.__handlers.get('onStart')
+        if not handler is None:
+            result = handler(event.text)
+            await event.answer(result,parse_mode = types.ParseMode.HTML)
+        else:
+            await event.answer(f"Hello {event.from_user.get_mention(as_html=True)} :-)",parse_mode = types.ParseMode.HTML)
+    async def __handler(self,event,regexp):
+            result = self.__handlers['text'](event.text)
+            await event.answer(result,parse_mode = types.ParseMode.HTML)
     def run(self):
         print('Bot run in polling mode. Please CTRL-Break for exit...')        
-        asyncio.run(self.__main())    
+        asyncio.run(self.__main())  
+        
+    def register_start_handler(self,handler=None):
+        self.__disp.register_message_handler(self.__start_handler,commands = {"start","restart"})
+        self.__handlers['onStart'] = handler
+        
+    def register_text_handler(self,handler,regexp):
+        self.__handlers["text"] = handler
+        self.__disp.register_message_handler(self.__handler,regexp = regexp)
