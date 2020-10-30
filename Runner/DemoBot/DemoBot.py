56ad9322f768b967e79b9aea9b6b052dcc1b06f3
Pavel Tsytovich <pavel.tsytovich@gmail.com>
2020-10-30 08:46:37 +0300

    рефакторинг кода

--- Runner/DemoBot/DemoBot.py
+++ Runner/DemoBot/DemoBot.py
@@ -21,24 +21,24 @@ class DemoBot:
         finally:
             await self.__bot.close()
                     
-    async def __start_handler(self,event):
-        handler = self.__handlers.get('onStart')
-        if not handler is None:
-            result = handler(event.text)
-            await event.answer(result,parse_mode = types.ParseMode.HTML)
-        else:
-            await event.answer(f"Hello {event.from_user.get_mention(as_html=True)} :-)",parse_mode = types.ParseMode.HTML)
-    async def __handler(self,event,regexp):
-            result = self.__handlers['text'](event.text)
-            await event.answer(result,parse_mode = types.ParseMode.HTML)
+   
     def run(self):
         print('Bot run in polling mode. Please CTRL-Break for exit...')        
         asyncio.run(self.__main())  
         
     def register_start_handler(self,handler=None):
-        self.__disp.register_message_handler(self.__start_handler,commands = {"start","restart"})
-        self.__handlers['onStart'] = handler
+        async def internal_handler(event):
+            if not handler is None:
+                result = handler(event.text)
+                await event.answer(result,parse_mode = types.ParseMode.HTML)
+            else:
+                await event.answer(f"Hello {event.from_user.get_mention(as_html=True)} :-)",parse_mode = types.ParseMode.HTML)
+             
+        self.__disp.register_message_handler(internal_handler,commands = {"start","restart"})
+      
         
-    def register_text_handler(self,handler,regexp):
-        self.__handlers["text"] = handler
-        self.__disp.register_message_handler(self.__handler,regexp = regexp)
+    def register_text_handler(self,handler,**aux):
+        async def internal__handler(event,regexp):
+                result = handler(event.text)            
+                await event.answer(result,parse_mode = types.ParseMode.HTML)                    
+        self.__disp.register_message_handler(internal__handler,**aux)
--- Runner/main.py
+++ Runner/main.py
@@ -7,7 +7,7 @@ import user
 def main():
     bot = DemoBot()
     bot.register_start_handler()
-    bot.register_text_handler(user.sample_text,'^[A-Za-z0-9]*')
+    bot.register_text_handler(user.sample_text,regexp='^[A-Za-z0-9]*')
     bot.run()
     
 if __name__ == '__main__':
