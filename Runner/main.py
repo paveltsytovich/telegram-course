0a4188e472fe4df7f6bfe55de440d771227af4ae
Pavel Tsytovich <pavel.tsytovich@gmail.com>
2020-10-30 08:28:45 +0300

    код пользователя теперь в отдельном файле

--- Runner/main.py
+++ Runner/main.py
@@ -2,14 +2,12 @@
 #
 #  Demo bot main file
 from DemoBot import DemoBot
-def sample_text(message):
-    print(message)
-    return message[::-1]
+import user
 
 def main():
     bot = DemoBot()
     bot.register_start_handler()
-    bot.register_text_handler(sample_text,'^[A-Za-z0-9]*')
+    bot.register_text_handler(user.sample_text,'^[A-Za-z0-9]*')
     bot.run()
     
 if __name__ == '__main__':
