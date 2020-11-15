639d8a05bdda70700870431ea4207d2c4bfe82fc
Pavel Tsytovich <pavel.tsytovich@gmail.com>
2020-11-15 16:53:05 +0300

    пример на вебхуках

---
+++ Code/Module 12/Live/simple-webhook-bot.py
@@ -0,0 +1,53 @@
+from aiogram import Bot, types
+from aiogram.contrib.middlewares.logging import LoggingMiddleware
+from aiogram.dispatcher import Dispatcher
+from aiogram.dispatcher.webhook import SendMessage
+from aiogram.utils.executor import start_webhook
+
+bot_token = '1205352313:AAHvh4X2cpF5TJCa2pTXb_YNORFvKyVzjb4'
+
+webhook_host = 'challenge.teachwizard.ru' 
+webhook_path = '/api/'
+webhook_url = f"{webhook_host}{webhook_path}"
+
+webapp_host = 'localhost'
+webapp_port = 3001
+
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher(bot)
+
+@dp.message_handler(commands=['start', 'help'])
+async def send_welcome(message: types.Message):
+    """
+    This handler will be called when user sends `/start` or `/help` command
+    """
+    await message.reply("Hi!\nI'm EchoBot!\nPowered by aiogram.")
+    
+@dp.message_handler()
+async def echo(message: types.Message):
+    # old style:
+    # await bot.send_message(message.chat.id, message.text)
+
+    await message.answer(message.text)
+
+async def on_startup(dp):
+    await bot.set_webhook(webhook_url)
+
+async def on_shutdown(dp):
+    logging.warning('Shutting down..')
+
+    await bot.delete_webhook()
+   
+    await dp.storage.close()
+    await dp.storage.wait_closed()
+
+  if __name__ == '__main__':
+    start_webhook(
+        dispatcher=dp,
+        webhook_path=webhook_path,
+        on_startup=on_startup,
+        on_shutdown=on_shutdown,
+        skip_updates=True,
+        host=webhook_host,
+        port=webapp_port,
+    )
