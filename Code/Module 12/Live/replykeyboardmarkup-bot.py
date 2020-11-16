73c2d4fe3237a206ad50160980a242c9235f1bfe
Pavel Tsytovich <pavel.tsytovich@gmail.com>
2020-11-16 18:47:19 +0300

    В примере обрабатываем теперь получение локации и контакта

--- Code/Module 12/Live/replykeyboardmarkup-bot.py
+++ Code/Module 12/Live/replykeyboardmarkup-bot.py
@@ -1,7 +1,7 @@
 from aiogram import Bot, Dispatcher, executor, types
 from aiogram.types import ReplyKeyboardRemove, \
     ReplyKeyboardMarkup, KeyboardButton, \
-    InlineKeyboardMarkup, InlineKeyboardButton
+    InlineKeyboardMarkup, InlineKeyboardButton,ContentType
     
 bot_token = '1205352313:AAHvh4X2cpF5TJCa2pTXb_YNORFvKyVzjb4'
  
@@ -17,16 +17,26 @@ async def one_keyboard(message: types.Message):
 
 
 @dp.message_handler(commands=['complex'])
-async def complex_keyboard(parameter_list):
-    
+async def complex_keyboard(message : types.Message):
+    button_text = KeyboardButton('Это просто текст')
+    button_contact = KeyboardButton('Отправь свой контакт',request_contact= True)
+    button_location = KeyboardButton('Отправь свое местонахождение',request_location=True)
+    kb = ReplyKeyboardMarkup(resize_keyboard=True)
+    kb.add(button_text)
+    kb.add(button_location)
+    kb.add(button_contact)
+    await message.answer('Нажми кнопку',reply_markup = kb)
     
 @dp.message_handler()
 async def echo(message: types.Message):
     # old style:
     # await bot.send_message(message.chat.id, message.text)
-
+    print(message)
     await message.answer(message.text)
     
-
+@dp.message_handler(content_types = [ContentType.CONTACT,ContentType.LOCATION] )
+async def get_contact(message : types.Message):
+    print(message)
+    await message.answer('Данные получены')
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
