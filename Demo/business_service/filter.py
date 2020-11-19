6fc8182bf6a9e50cf190818e1a430c908f1ce9cb
Pavel Tsytovich <pavel.tsytovich@gmail.com>
2020-11-19 18:22:25 +0300

    теперь фильтр работает и с русским языком

--- Demo/business_service/filter.py
+++ Demo/business_service/filter.py
@@ -3,7 +3,7 @@ import os
 def filter_handler(message):
     result = message
     path = os.getcwd() + '\\data\\'
-    with open(path + 'bad_words.txt') as b:
+    with open(path + 'bad_words.txt',encoding='utf-8') as b:
         bads = [ c[:len(c)-1] for c in b]
     for c in bads:
         result = result.replace(c,'***')
