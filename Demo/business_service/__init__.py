5cc02b962aedec8fcf6a2db5ef418a87e7243942
Pavel Tsytovich <pavel.tsytovich@gmail.com>
2020-11-22 18:41:52 +0300

    начали с NoSQL

--- Demo/business_service/__init__.py
+++ Demo/business_service/__init__.py
@@ -3,3 +3,4 @@ from .filter import filter_handler
 from .regexp import *
 from .rel_db import init
 from .rel_db import train_schedule
+from .nosql_db import init_nosql
---
+++ Demo/business_service/nosql_db.py
@@ -0,0 +1,2 @@
+def init_nosql():
+    pass
