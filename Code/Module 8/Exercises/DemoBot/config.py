14a167e4ea0e294704c67165896310e151576241
Pavel Tsytovich <pavel.tsytovich@gmail.com>
2020-12-05 17:02:06 +0300

    Удален ИД бота из конфигов упражений

--- Code/Module 8/Exercises/DemoBot/config.py
+++ Code/Module 8/Exercises/DemoBot/config.py
@@ -3,7 +3,7 @@
 # demo bot config file
 import os
 
-bot_id = os.environ.get('BOT_ID','1205352313:AAHvh4X2cpF5TJCa2pTXb_YNORFvKyVzjb4')
+bot_id = os.environ.get('BOT_ID','**********************************************')
 connection_param={'dbname':'train-schedule','user':'bot','password':'qwerty','host':'127.0.0.1'}
 mongo_connection = 'mongodb://localhost:27017'
 ws = 'http://cbu.uz/ru/arkhiv-kursov-valyut/json/RUB/'
